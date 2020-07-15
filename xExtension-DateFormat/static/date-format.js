f680df9054ae7ff121a9936090f35353238538e2
Alexis Degrugillier <github@ainw.org>
2020-07-15 21:22:34 +0200

    Improve performances while formatting dates
    The dateTime property is removed after processing to limit the number of
    dates to process after loading more content.

--- xExtension-DateFormat/static/date-format.js
+++ xExtension-DateFormat/static/date-format.js
@@ -1,40 +1,38 @@
-const adjustedDates = function () {
-    document.querySelectorAll('time[datetime]').forEach(e => {
-        let content = e.innerHTML;
-        let date = moment(e.dateTime);
-        let zoneDate = moment.parseZone(e.dateTime);
-        content = content.replace(zoneDate.format('HH:mm'), date.format('HH:mm'));
-        content = content.replace(zoneDate.format('MMMM'), date.format('MMMM'));
-        content = content.replace(zoneDate.format('D'), date.format('D'));
-        content = content.replace(zoneDate.format('YYYY'), date.format('YYYY'));
-        content = content.replace(zoneDate.format('Do').replace(/[0-9]/g, ''), date.format('Do').replace(/[0-9]/g, ''));
-        e.innerHTML = content;
-    });
+const adjustedDates = function (e) {
+    let content = e.innerHTML;
+    let date = moment(e.dateTime);
+    let zoneDate = moment.parseZone(e.dateTime);
+    content = content.replace(zoneDate.format('HH:mm'), date.format('HH:mm'));
+    content = content.replace(zoneDate.format('MMMM'), date.format('MMMM'));
+    content = content.replace(zoneDate.format('D'), date.format('D'));
+    content = content.replace(zoneDate.format('YYYY'), date.format('YYYY'));
+    content = content.replace(zoneDate.format('Do').replace(/[0-9]/g, ''), date.format('Do').replace(/[0-9]/g, ''));
+    e.innerHTML = content;
 }
 
-const relativeDates = function () {
-    document.querySelectorAll('time[datetime]').forEach(e => {
-        e.innerHTML = moment(e.dateTime).fromNow();
-    });
+const relativeDates = function (e) {
+    e.innerHTML = moment(e.dateTime).fromNow();
 }
 
-const hours12 = function () {
-    document.querySelectorAll('time[datetime]').forEach(e => {
-        let content = e.innerHTML;
-        let date = dateFormatConfiguration.adjustedDates ? moment(e.dateTime) : moment.parseZone(e.dateTime);
-        let zoneDate = moment.parseZone(e.dateTime);
-        e.innerHTML = content.replace(zoneDate.format('HH:mm'), date.format('hh:mm A'));
-    });
+const hours12 = function (e) {
+    let content = e.innerHTML;
+    let date = dateFormatConfiguration.adjustedDates ? moment(e.dateTime) : moment.parseZone(e.dateTime);
+    let zoneDate = moment.parseZone(e.dateTime);
+    e.innerHTML = content.replace(zoneDate.format('HH:mm'), date.format('hh:mm A'));
 }
 
 const dateFormat = function () {
-    if (dateFormatConfiguration.relativeDates) {
-        relativeDates();
-    } else if (dateFormatConfiguration.hour12Dates) {
-        hours12();
-    } else if (dateFormatConfiguration.adjustedDates) {
-        adjustedDates();
-    }
+    document.querySelectorAll('time[datetime]').forEach(e => {
+        if (dateFormatConfiguration.relativeDates) {
+            relativeDates(e);
+        } else if (dateFormatConfiguration.hour12Dates) {
+            hours12(e);
+        } else if (dateFormatConfiguration.adjustedDates) {
+            adjustedDates(e);
+        }
+
+        e.removeAttribute('dateTime');
+    });
 }
 
 window.onload = function () {
